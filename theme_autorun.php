6a727bcd0856463995f7c5ece31bf1514185df8b
Tad <tad0616@gmail.com>
2021-01-19 11:21:46 +0800

    修正首頁無內容時，下拉選單無法正確運作問題

--- theme_autorun.php
+++ theme_autorun.php
@@ -1,4 +1,7 @@
 <?php
+use XoopsModules\Tadtools\Utility;
+Utility::get_jquery(true);
+
 if (file_exists(XOOPS_ROOT_PATH . "/class/xoopsform/renderer/XoopsFormRendererBootstrap4.php")) {
     xoops_load('XoopsFormRendererBootstrap4');
     XoopsFormRenderer::getInstance()->set(new XoopsFormRendererBootstrap4());
