9ad87648d72f0a8e15dfca25a8d88355a9ddfb60
Tad <tad0616@gmail.com>
2021-01-22 09:05:19 +0800

    變更 jquery 引入方式

--- theme_autorun.php
+++ theme_autorun.php
@@ -1,7 +1,9 @@
 <?php
 use XoopsModules\Tadtools\Utility;
+if (!class_exists('XoopsModules\Tadtools\Utility')) {
+    require XOOPS_ROOT_PATH . '/modules/tadtools/preloads/autoloader.php';
+}
 Utility::get_jquery(true);
-
 if (file_exists(XOOPS_ROOT_PATH . "/class/xoopsform/renderer/XoopsFormRendererBootstrap4.php")) {
     xoops_load('XoopsFormRendererBootstrap4');
     XoopsFormRenderer::getInstance()->set(new XoopsFormRendererBootstrap4());
